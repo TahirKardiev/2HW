222aefb48e6d6352b3b597976f20f263d277cdd2
Andrew <mr.fesenko@inbox.ru>
2022-08-19 15:21:01 +0300

    added t1

---
+++ Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿double a, c; string S;
+Console.WriteLine("Enter a number "); S = Console.ReadLine ();
+a = double.Parse (S);
+Console.WriteLine("Enter numbers power"); S = Console.ReadLine ();
+c = double.Parse (S);
+Console.WriteLine(Math.Pow(a, c));
