9bce4fe5ca04554801e223f440fa4d7e12d3f8d8
ФЕСС - <mr.fesenko@inbox.ru>
2022-08-21 16:31:22 +0300

    added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿double n, sum, dig ; string S; Console.WriteLine("Enter a number "); S = Console.ReadLine ();
+n = double.Parse (S);
+sum = 0;
+dig = 0;
+while(n > 0)
+{
+    if (n > 9)
+    {
+    dig = n % 10;
+    }
+    else 
+    {
+        dig = n;
+    }
+sum = sum + dig;
+n = (n-dig)/10;
+}
+
+Console.WriteLine ($"Сумма цифр равна:{sum}");
